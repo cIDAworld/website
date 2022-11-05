165c4ed9f3eaf260eb1aaa6733bea7830a27299f
SFRL <sebastian.lobbers@googlemail.com>
2022-11-05 17:55:22 +0000

    update log update script

--- log/update_log.py
+++ log/update_log.py
@@ -8,12 +8,7 @@ log_dir = os.path.join(dir, 'log.txt')
 # Get datetime
 dt = datetime.datetime.now()
 
-# Open log
-with open(log_dir) as f:
-    log_txt = f.read()
-
-
-new_log_txt = "- {}\n{}".format(dt, log_txt)
+new_log_txt = "The last scheduled commit was on:\n{}".format(dt)
 
 with open(log_dir, 'w') as f:
     f.write(new_log_txt)
